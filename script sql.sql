d87fc235d6f3d5c8985c844b1ec4258c49113979
FaheemESILV <117808658+FaheemESILV@users.noreply.github.com>
2022-11-29 11:57:05 +0100

    Update script sql.sql

--- script sql.sql
+++ script sql.sql
@@ -7,46 +7,53 @@ DROP TABLE IF EXISTS licence;
 DROP TABLE IF EXISTS car;
 
 
-CREATE TABLE car (matriculation VARCHAR(150) NOT NULL PRIMARY KEY, 
+CREATE TABLE car (
+ matriculation VARCHAR(150) NOT NULL PRIMARY KEY, 
 year INT(4), 
 color VARCHAR(150),
  idres BIGINT(50), 
  idcategory BIGINT(50) , 
- idmodel BIGINT(50) );
+ idmodel BIGINT(50) )ENGINE=INNODB;
 
-CREATE TABLE client (idclient BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+CREATE TABLE client (
+ idclient BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY, 
 name VARCHAR(150), 
 firstname VARCHAR(150),
  birth DATE,
  adress VARCHAR(150), 
  city VARCHAR(150),
  idres BIGINT(50),
- idlicence VARCHAR(150));
+ idlicence VARCHAR(150))ENGINE=INNODB;
 
-CREATE TABLE reservation (idres BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE reservation (
+ idres BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY,
  date_start DATE,
  date_end DATE,
  km_start INT, 
  km_end INT,
- matriculation VARCHAR(150));
+ matriculation VARCHAR(150))ENGINE=INNODB;
 
-CREATE TABLE category (idcategory BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY,
- category_name VARCHAR(150));
+CREATE TABLE category (
+ idcategory BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+ category_name VARCHAR(150))ENGINE=INNODB;
 
-CREATE TABLE licence (idlicence VARCHAR(150) NOT NULL PRIMARY KEY, 
+CREATE TABLE licence (
+ idlicence VARCHAR(150) NOT NULL PRIMARY KEY, 
 date_licence DATE,
  young_driver BOOLEAN, 
- idclient BIGINT(50));
+ idclient BIGINT(50))ENGINE=INNODB;
 
-CREATE TABLE fuel (idfuel BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY, 
-fuel_name VARCHAR(150));
+CREATE TABLE fuel (
+ idfuel BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+fuel_name VARCHAR(150))ENGINE=INNODB;
 
-CREATE TABLE model (idmodel BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE model (
+ idmodel BIGINT(50) AUTO_INCREMENT NOT NULL PRIMARY KEY,
  power INT,
  transmission VARCHAR(150),
  seats INT,
  brand VARCHAR(150), 
- model_name VARCHAR(150));
+ model_name VARCHAR(150))ENGINE=INNODB;
 
 
 ALTER TABLE car ADD CONSTRAINT FK_car_idres FOREIGN KEY (idres) REFERENCES reservation (idres);
